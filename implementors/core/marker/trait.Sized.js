e950761240cc83673d20679db1c0e78ea14d8aac
brunoczim <brunoczim@users.noreply.github.com>
2022-05-27 22:39:34 +0000

    deploy: cceb79f02c73cee0def73e1be0696fc26b395863

--- implementors/core/marker/trait.Sized.js
+++ implementors/core/marker/trait.Sized.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["fslock"] = [{"text":"impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/1.59.0/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a> for <a class=\"struct\" href=\"fslock/struct.OsStr.html\" title=\"struct fslock::OsStr\">OsStr</a>","synthetic":true,"types":["fslock::unix::OsStr"]}];
+implementors["fslock"] = [{"text":"impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/1.61.0/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a> for <a class=\"struct\" href=\"fslock/struct.OsStr.html\" title=\"struct fslock::OsStr\">OsStr</a>","synthetic":true,"types":["fslock::unix::OsStr"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
